deb5f28ed91938266045966e3e6986f8254f7653
konman1989 <manoilo.blog@gmail.com>
2020-05-31 19:54:54 +0300

    implement routs for apps

---
+++ social/urls.py
@@ -0,0 +1,34 @@
+"""social URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import UserCreateView
+from posts.views import PostLikesAnalyticsView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/analytics/', PostLikesAnalyticsView.as_view(), name='analytics'),
+    path('api/register/', UserCreateView.as_view(), name='user_register'),
+    path('api/users/', include('users.urls', 'users_api')),
+    path('api/posts/', include('posts.urls', 'posts_api')),
+]
